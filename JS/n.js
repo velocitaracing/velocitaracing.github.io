7a284fd05c1c506f8635b7b38b81b649d78e9d54
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-10-20 13:52:43 +0530

    changes_13

---
+++ JS/n.js
@@ -0,0 +1,21 @@
+// const Slide = () =>{
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.navbar');
+    
+
+//             // toggle
+//     burger.addEventListener('click',()=>{
+//         nav.classList.toggle('black');
+//             // nav-animation
+//             // burger-animation
+//         burger.classList.toggle('toggle');    
+
+//     });
+
+
+
+
+    
+// }
+
+// Slide();
