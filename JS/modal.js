8404ea670364b58465da4c107bbf565b52fede70
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-10-22 01:45:52 +0530

    changes_19

---
+++ JS/modal.js
@@ -0,0 +1,52 @@
+var modalButton = document.querySelector('.modal-btn-one');
+var modalBg = document.querySelector('.modal-bg-one');
+var modalClose = document.querySelector('.modal-close-one');
+
+modalButton.addEventListener('click', function(){
+    modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+});
+
+
+var modalButtontwo = document.querySelector('.modal-btn-two');
+var modalBgtwo = document.querySelector('.modal-bg-two');
+var modalClosetwo = document.querySelector('.modal-close-two');
+
+modalButtontwo.addEventListener('click', function(){
+    modalBgtwo.classList.add('bg-active');
+});
+
+modalClosetwo.addEventListener('click', function(){
+    modalBgtwo.classList.remove('bg-active');
+});
+
+
+var modalButtontwo = document.querySelector('.modal-btn-three');
+var modalBgtwo = document.querySelector('.modal-bg-two');
+var modalClosetwo = document.querySelector('.modal-close-two');
+
+modalButtontwo.addEventListener('click', function(){
+    modalBgtwo.classList.add('bg-active');
+});
+
+modalClosetwo.addEventListener('click', function(){
+    modalBgtwo.classList.remove('bg-active');
+});
+
+
+var modalButtontwo = document.querySelector('.modal-btn-four');
+var modalBgtwo = document.querySelector('.modal-bg-two');
+var modalClosetwo = document.querySelector('.modal-close-two');
+
+modalButtontwo.addEventListener('click', function(){
+    modalBgtwo.classList.add('bg-active');
+});
+
+modalClosetwo.addEventListener('click', function(){
+    modalBgtwo.classList.remove('bg-active');
+});
+
+
