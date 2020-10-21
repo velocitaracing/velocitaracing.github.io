6edbfee3ab6cf7836ae1f4f69af79a15cedac5be
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-10-21 21:02:36 +0530

    changes_15

---
+++ JS/blog.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+      triggerSlideIns( $(this), [ $('div.itm'), $('div.info-sec'), $('div.ant-itm'), $('div.VR-1') ]);
+    });
+  });
+  
+  $.getDocHeight = function(){
+    return Math.max(
+        $(document).height(),
+        $(window).height(),
+        document.documentElement.clientHeight
+    );
+  };
+  
+  $.getScrollPercentage = function(){
+    return 100 * Math.min(
+      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+      $(window).scrollTop()
+      );
+  };
+  
+  var triggerSlideIns = function(t, items) {
+    
+    for (var i = 0; i < items.length; i++) {
+      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+        if(!items[i].hasClass('transitionSlideIn'))
+        {
+          items[i].addClass('transitionSlideIn');
+        }
+      } else { items[i].removeClass('transitionSlideIn'); }
+    }
+  };
+
+
+
+
+  
+  $(document).ready(function(){
+    $(window).scroll(function () {
+      triggerSlideInse( $(this), [ $('div.info'), $('div.itm-sec'),  $('div.VR-2')]);
+    });
+  });
+  
+  $.getDocHeight = function(){
+    return Math.max(
+        $(document).height(),
+        $(window).height(),
+        document.documentElement.clientHeight
+    );
+  };
+  
+  $.getScrollPercentage = function(){
+    return 100 * Math.min(
+      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+      $(window).scrollTop()
+      );
+  };
+  
+  var triggerSlideInse = function(t, items) {
+    
+    for (var i = 0; i < items.length; i++) {
+      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+        if(!items[i].hasClass('transitionSlideInright'))
+        {
+          items[i].addClass('transitionSlideInright');
+        }
+      } else { items[i].removeClass('transitionSlideInright'); }
+    }
+  };
+
---
+++ JS/scrollslide.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+      triggerSlideInt( $(this), [ $('div.about-left-one'), $('div.car-image-left'), $('div.left-info'),$('div.left-one'), $('div.left-two')]);
+    });
+  });
+  
+  $.getDocHeight = function(){
+    return Math.max(
+        $(document).height(),
+        $(window).height(),
+        document.documentElement.clientHeight
+    );
+  };
+  
+  $.getScrollPercentage = function(){
+    return 100 * Math.min(
+      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+      $(window).scrollTop()
+      );
+  };
+  
+  var triggerSlideInt = function(t, items) {
+    
+    for (var i = 0; i < items.length; i++) {
+      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+        if(!items[i].hasClass('transitionSlideIn'))
+        {
+          items[i].addClass('transitionSlideIn');
+        }
+      } else { items[i].removeClass('transitionSlideIn'); }
+    }
+  };
+
+
+
+
+  
+  $(document).ready(function(){
+    $(window).scroll(function () {
+      triggerSlideInsu( $(this), [ $('div.right-one'), $('div.right-two'), $('div.right-info'),$('div.car-image-right')]);
+    });
+  });
+  
+  $.getDocHeight = function(){
+    return Math.max(
+        $(document).height(),
+        $(window).height(),
+        document.documentElement.clientHeight
+    );
+  };
+  
+  $.getScrollPercentage = function(){
+    return 100 * Math.min(
+      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+      $(window).scrollTop()
+      );
+  };
+  
+  var triggerSlideInsu = function(t, items) {
+    
+    for (var i = 0; i < items.length; i++) {
+      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+        if(!items[i].hasClass('transitionSlideInright'))
+        {
+          items[i].addClass('transitionSlideInright');
+        }
+      } else { items[i].removeClass('transitionSlideInright'); }
+    }
+  };
+
