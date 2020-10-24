3e98a0d52e9209a74d881eeb79200e2dfc8d63d1
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-10-24 18:58:42 +0530

    changes_34

---
+++ JS/circle-loader.js
@@ -0,0 +1,34 @@
+$(document).ready(function ($) {
+    function animateElements() {
+        $('.progressbar').each(function () {
+            var elementPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            var percent = $(this).find('.circle').attr('data-percent');
+            var percentage = parseInt(percent, 10) / parseInt(100, 10);
+            var animate = $(this).data('animate');
+            if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+                $(this).data('animate', true);
+                $(this).find('.circle').circleProgress({
+                    startAngle: -Math.PI / 2,
+                    value: percent / 100,
+                    size: 180,
+                    thickness: 30,
+                    emptyFill: "rgba(0,0,0, .2)",
+                    fill: {
+                        color: '#fb4f14'
+                    }
+                }).on('circle-animation-progress', function (event, progress, stepValue) {
+                    $(this).find('div').text((stepValue*100).toFixed(1) + "%");
+                }).stop();
+            }
+        });
+    }
+
+    // Show animated elements
+    animateElements();
+    $(window).scroll(animateElements);
+    
+    
+    
+
+}); //end document ready function
