b306d4f10c353576349a2de05cfe6256b567acfc
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-10-30 16:01:08 +0530

    changes_84

--- JS/ripple.js
+++ JS/ripple.js
@@ -1,35 +1,3 @@
-// const btn = document.querySelector(".ripple-btn"); 
-  
-// // Listen for click event 
-// btn.onclick = function (e) { 
-
-//     // Create span element 
-//     let ripple = document.createElement("span"); 
-
-//     // Add ripple class to span 
-//     ripple.classList.add("ripple"); 
-
-//     // Add span to the button  
-//     this.appendChild(ripple); 
-
-//     // Get position of X 
-//     let x = e.clientX - e.target.offsetLeft; 
-
-//     // Get position of Y  
-//     let y = e.clientY - e.target.offsetTop; 
-
-//     // Position the span element 
-//     ripple.style.left = `${x}px`; 
-//     ripple.style.top = `${y}px`; 
-
-//     // Remove span after 0.3s 
-//     setTimeout(() => { 
-//         ripple.remove(); 
-//     }, 300); 
-
-// }; 
-
-
 
 $(".ripple-btn").click(function (e) {
 
