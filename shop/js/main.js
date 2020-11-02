0bb01cf74d48cfc9694068349225f7b94ddcb6be
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-11-02 14:40:39 +0530

    shop

--- shop/js/main.js
+++ shop/js/main.js
@@ -115,5 +115,18 @@ $(window).scroll(function(e){
 });
 
 
+$(window).on('scroll', function(){
+  if($(window).scrollTop()){
+      $('.navbar').addClass('black');
+  }
+  else{
+      $('.navbar').removeClass('black');
+  }
+  
+})
+
+
+
+
 });
 
