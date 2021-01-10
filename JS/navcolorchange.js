121ac7a09346329778ce34ad5b32c51ca8534c22
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-01-10 13:24:29 +0530

    updates

--- JS/navcolorchange.js
+++ JS/navcolorchange.js
@@ -2,10 +2,12 @@ $(window).on('scroll', function(){
     if($(window).scrollTop()){
         $('.navbar').addClass('black');
         $('.navbar').addClass('animate-nav');
+        $('.goto-top-link').addClass('visible');
     }
     else{
         $('.navbar').removeClass('black');
         $('.navbar').removeClass('animate-nav');
+        $('.goto-top-link').removeClass('visible');
     }
     
 })
--- JS/smoothscroll.js
+++ JS/smoothscroll.js
@@ -59,6 +59,26 @@ if(this.hash !== '') {
 
 });
 
+$('.goto-top-link').on('click', function(e) {
+  // console.log(this);
+
+if(this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+            scrollTop: $(hash).offset().top
+          },
+            800
+    );
+       
+  }
+
+});
+
+
 
 
 
