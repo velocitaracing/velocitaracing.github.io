c93c3323426f890ec06c800d43879c2cba7365f9
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-03-27 12:21:45 +0530

    Update scrollslide.js

--- JS/scrollslide.js
+++ JS/scrollslide.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
+
+
+// $(window).resize(function() {
+  if( $(window).width() > 500 ) {
+      // code
+
+  
+  $(document).ready(function(){
     $(window).scroll(function () {
       triggerSlideInt( $(this), [ $('div.about-left-one'), $('div.car-image-left'), $('div.left-info'),$('div.left-one'), $('div.left-two'), $('div.career-left'), $('div.sponsor-left')]);
     });
@@ -67,4 +74,46 @@ $(document).ready(function(){
       } else { items[i].removeClass('transitionSlideInright'); }
     }
   };
+
+
+
+}
+
+else{
+  $(document).ready(function(){
+    $(window).scroll(function () {
+      triggerSlideInsb( $(this), [ $('div.about-left-one'), $('div.team-box'), $('div.team-right'), $('div.joinus-right'), $('div.career-left'), $('div.sponsor-left')]);
+    });
+  });
+  
+  $.getDocHeight = function(){
+    return Math.max(
+        $(document).height(),
+        $(window).height(),
+        document.documentElement.clientHeight
+    );
+  };
+  
+  $.getScrollPercentage = function(){
+    return 100 * Math.min(
+      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+      $(window).scrollTop()
+      );
+  };
+  
+  var triggerSlideInsb = function(t, items) {
+    
+    for (var i = 0; i < items.length; i++) {
+      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+        if(!items[i].hasClass('transitionSlideInbottomt'))
+        {
+          items[i].addClass('transitionSlideInbottom');
+        }
+      } else { items[i].removeClass('transitionSlideInbottom'); }
+    }
+  };
+
+}
+
+// });
 
