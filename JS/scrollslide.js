0519bfe878facba2c2934dbebdffe507fdc66381
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-04-23 20:18:39 +0530

    updates

--- JS/scrollslide.js
+++ JS/scrollslide.js
@@ -1,83 +1,83 @@
 
 
 // $(window).resize(function() {
-  if( $(window).width() > 500 ) {
-      // code
+//   if( $(window).width() > 500 ) {
+//       // code
 
   
-  $(document).ready(function(){
-    $(window).scroll(function () {
-      triggerSlideInt( $(this), [ $('div.left-info'), $('div.career-left'), $('div.sponsor-left')]);
-    });
-  });
+//   $(document).ready(function(){
+//     $(window).scroll(function () {
+//       triggerSlideInt( $(this), [ $('div.left-info'), $('div.career-left'), $('div.sponsor-left')]);
+//     });
+//   });
   
-  $.getDocHeight = function(){
-    return Math.max(
-        $(document).height(),
-        $(window).height(),
-        document.documentElement.clientHeight
-    );
-  };
+//   $.getDocHeight = function(){
+//     return Math.max(
+//         $(document).height(),
+//         $(window).height(),
+//         document.documentElement.clientHeight
+//     );
+//   };
   
-  $.getScrollPercentage = function(){
-    return 100 * Math.min(
-      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
-      $(window).scrollTop()
-      );
-  };
+//   $.getScrollPercentage = function(){
+//     return 100 * Math.min(
+//       ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+//       $(window).scrollTop()
+//       );
+//   };
   
-  var triggerSlideInt = function(t, items) {
+//   var triggerSlideInt = function(t, items) {
     
-    for (var i = 0; i < items.length; i++) {
-      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
-        if(!items[i].hasClass('transitionSlideIn'))
-        {
-          items[i].addClass('transitionSlideIn');
-        }
-      } else { items[i].removeClass('transitionSlideIn'); }
-    }
-  };
+//     for (var i = 0; i < items.length; i++) {
+//       if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+//         if(!items[i].hasClass('transitionSlideIn'))
+//         {
+//           items[i].addClass('transitionSlideIn');
+//         }
+//       } else { items[i].removeClass('transitionSlideIn'); }
+//     }
+//   };
 
 
 
 
   
-  $(document).ready(function(){
-    $(window).scroll(function () {
-      triggerSlideInsu( $(this), [ $('div.right-info'), $('div.team-right'), $('div.joinus-right')]);
-    });
-  });
+//   $(document).ready(function(){
+//     $(window).scroll(function () {
+//       triggerSlideInsu( $(this), [ $('div.right-info'), $('div.team-right'), $('div.joinus-right')]);
+//     });
+//   });
   
-  $.getDocHeight = function(){
-    return Math.max(
-        $(document).height(),
-        $(window).height(),
-        document.documentElement.clientHeight
-    );
-  };
+//   $.getDocHeight = function(){
+//     return Math.max(
+//         $(document).height(),
+//         $(window).height(),
+//         document.documentElement.clientHeight
+//     );
+//   };
   
-  $.getScrollPercentage = function(){
-    return 100 * Math.min(
-      ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
-      $(window).scrollTop()
-      );
-  };
+//   $.getScrollPercentage = function(){
+//     return 100 * Math.min(
+//       ($(window).height() + $(window).scrollTop()) / $.getDocHeight(),
+//       $(window).scrollTop()
+//       );
+//   };
   
-  var triggerSlideInsu = function(t, items) {
+//   var triggerSlideInsu = function(t, items) {
     
-    for (var i = 0; i < items.length; i++) {
-      if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
-        if(!items[i].hasClass('transitionSlideInright'))
-        {
-          items[i].addClass('transitionSlideInright');
-        }
-      } else { items[i].removeClass('transitionSlideInright'); }
-    }
-  };
+//     for (var i = 0; i < items.length; i++) {
+//       if((( $.getDocHeight() - $(window).height()) - ( $.getDocHeight() - items[i].offset().top )) <= t.scrollTop()) {
+//         if(!items[i].hasClass('transitionSlideInright'))
+//         {
+//           items[i].addClass('transitionSlideInright');
+//         }
+//       } else { items[i].removeClass('transitionSlideInright'); }
+//     }
+//   };
 
 
 
-}
+// }
 
 // else{
 //   $(document).ready(function(){
