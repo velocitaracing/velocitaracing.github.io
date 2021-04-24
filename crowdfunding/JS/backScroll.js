85b2cde0be7d55a5da4560f2808d95e09af7921e
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-04-25 03:22:56 +0530

    updates

--- crowdfunding/JS/backScroll.js
+++ crowdfunding/JS/backScroll.js
@@ -4,8 +4,9 @@ function update(){
     $('#home').each(function() { 
         var $element = $(this);
         // subtract some from the height b/c of the padding
-        var height = 0;
-        $(this).css('backgroundPosition', '50% ' + (pos * velocity) +  'px');
+        var scrollHeight = $('#home').position().top;
+        var height = scrollHeight;
+        $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px'); 
         // $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px'); 
        }); 
        };
