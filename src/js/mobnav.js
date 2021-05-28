6536fddb64fcb5538c411c877a71a94e3fd9d307
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-05-28 13:15:44 +0530

    fixes

--- src/js/mobnav.js
+++ src/js/mobnav.js
@@ -2,25 +2,18 @@ const burger = document.getElementById('burger');
 const navUL = document.getElementById('nav-ul');
 // const body = document.querySelector('body');
 
-
 document.onclick = function (e){
     if(e.target.id !== 'burger' && (e.target.id !== 'nav-ul' && ( ( (e.target.className !== 'main-link') & (e.target.className !== 'navlink-border') & (e.target.className !== 'main-nav-icon') ) )))
     {
         navUL.classList.remove('nav-active');
         burger.classList.remove('toggle');
-        // body.classList.remove('nav-active-body');
+        
     }
-
-    // console.log(e.target.className)
 }
-
-
-
 
 burger.onclick = function() {
     navUL.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
-    // body.classList.toggle('nav-active-body');
 }
 
 
