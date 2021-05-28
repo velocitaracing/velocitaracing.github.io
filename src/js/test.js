fd58e09218c3490fc8f48be1fbdded7a7923888c
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-05-28 23:13:11 +0530

    fixes

--- src/js/test.js
+++ src/js/test.js
@@ -27,3 +27,83 @@ button.onclick = function (e) {
     }, 3100);
     
 }
+
+
+
+const subs = document.getElementById('support');
+const inp = document.getElementById('inp');
+const tik = document.getElementById('tick');
+
+subs.onclick = function (e) {
+    subs.style.color = 'transparent'
+
+    setTimeout(function(){
+       
+        subs.style.width = '42px'
+    }, 100);
+    
+
+    setTimeout(function(){
+        $("#support").addClass("animationButton");
+    }, 500);
+
+    setTimeout(function(){
+        $("#support").removeClass("animationButton");
+    }, 3000);
+
+    setTimeout(function(){
+        subs.style.width = '140px'
+    }, 3000);
+
+    setTimeout(function(){
+        subs.style.color = '#fff'
+    }, 3100);
+
+    setTimeout(function(){
+        subs.style.display  = 'none'
+        inp.style.display  = 'block'
+        tik.style.display  = 'block'
+        inp.style.width = 'unset'
+    }, 3200);   
+}
+
+tik.onclick = function (e) {
+    inp.style.display  = 'none'
+    tik.style.display  = 'none'
+    subs.style.display  = 'unset'
+    subs.style.color = 'transparent'
+    subs.style.width = '42px'
+
+
+    setTimeout(function(){
+      
+        inp.style.width = '42px'
+        $("#support").addClass("animationButton");
+    }, 100);
+
+    setTimeout(function(){
+        $("#support").removeClass("animationButton");
+    }, 3000);
+
+
+    setTimeout(function(){
+        subs.style.width = '150px'
+    }, 3100);   
+
+    setTimeout(function(){
+        subs.style.color = '#fff'
+    }, 3400);
+    
+    setTimeout(function(){
+        subs.innerHTML = "DONE";
+    }, 3400);
+
+    setTimeout(function(){
+        subs.innerHTML = "SUBSCRIBE";
+    }, 5000); 
+
+    
+}
+
+
+
