51d9412536806fe53663b80df413ee7ef300a059
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-06-24 19:22:41 +0530

    fixes

--- src/js/test.js
+++ src/js/test.js
@@ -1,109 +1,109 @@
-const button = document.getElementById('load');
-const rating = document.getElementById('rating');
+// const button = document.getElementById('load');
+// const rating = document.getElementById('rating');
 
-button.onclick = function (e) {
-    button.style.color = 'transparent'
+// button.onclick = function (e) {
+//     button.style.color = 'transparent'
 
-    setTimeout(function(){
+//     setTimeout(function(){
        
-        button.style.width = '38px'
-    }, 100);
+//         button.style.width = '38px'
+//     }, 100);
     
 
-    setTimeout(function(){
-        $("#load").addClass("animationButton");
-    }, 500);
+//     setTimeout(function(){
+//         $("#load").addClass("animationButton");
+//     }, 500);
 
-    setTimeout(function(){
-        $("#load").removeClass("animationButton");
-    }, 3000);
+//     setTimeout(function(){
+//         $("#load").removeClass("animationButton");
+//     }, 3000);
 
-    setTimeout(function(){
-        button.style.width = '140px'
-    }, 3000);
+//     setTimeout(function(){
+//         button.style.width = '140px'
+//     }, 3000);
 
-    setTimeout(function(){
-        button.style.color = '#fff'
-    }, 3100);
+//     setTimeout(function(){
+//         button.style.color = '#fff'
+//     }, 3100);
     
-}
+// }
 
 
 
-const subs = document.getElementById('support');
-const inp = document.getElementById('inp');
-const tik = document.getElementById('tick');
+// const subs = document.getElementById('support');
+// const inp = document.getElementById('inp');
+// const tik = document.getElementById('tick');
 
-subs.onclick = function (e) {
-    subs.style.color = 'transparent'
+// subs.onclick = function (e) {
+//     subs.style.color = 'transparent'
 
-    setTimeout(function(){
+//     setTimeout(function(){
        
-        subs.style.width = '42px'
-    }, 100);
+//         subs.style.width = '42px'
+//     }, 100);
     
 
-    setTimeout(function(){
-        $("#support").addClass("animationButton");
-    }, 500);
+//     setTimeout(function(){
+//         $("#support").addClass("animationButton");
+//     }, 500);
 
-    setTimeout(function(){
-        $("#support").removeClass("animationButton");
-    }, 3000);
+//     setTimeout(function(){
+//         $("#support").removeClass("animationButton");
+//     }, 3000);
 
-    setTimeout(function(){
-        subs.style.width = '140px'
-    }, 3000);
+//     setTimeout(function(){
+//         subs.style.width = '140px'
+//     }, 3000);
 
-    setTimeout(function(){
-        subs.style.color = '#fff'
-    }, 3100);
+//     setTimeout(function(){
+//         subs.style.color = '#fff'
+//     }, 3100);
 
-    setTimeout(function(){
-        subs.style.display  = 'none'
-        inp.style.display  = 'block'
-        tik.style.display  = 'block'
-        inp.style.width = 'unset'
-    }, 3200);   
-}
+//     setTimeout(function(){
+//         subs.style.display  = 'none'
+//         inp.style.display  = 'block'
+//         tik.style.display  = 'block'
+//         inp.style.width = 'unset'
+//     }, 3200);   
+// }
 
-tik.onclick = function (e) {
-    inp.style.display  = 'none'
-    tik.style.display  = 'none'
-    subs.style.display  = 'unset'
-    subs.style.color = 'transparent'
-    subs.style.width = '42px'
+// tik.onclick = function (e) {
+//     inp.style.display  = 'none'
+//     tik.style.display  = 'none'
+//     subs.style.display  = 'unset'
+//     subs.style.color = 'transparent'
+//     subs.style.width = '42px'
 
 
-    setTimeout(function(){
+//     setTimeout(function(){
       
-        inp.style.width = '42px'
-        $("#support").addClass("animationButton");
-    }, 100);
+//         inp.style.width = '42px'
+//         $("#support").addClass("animationButton");
+//     }, 100);
 
-    setTimeout(function(){
-        $("#support").removeClass("animationButton");
-    }, 3000);
+//     setTimeout(function(){
+//         $("#support").removeClass("animationButton");
+//     }, 3000);
 
 
-    setTimeout(function(){
-        subs.style.width = '150px'
-    }, 3100);   
+//     setTimeout(function(){
+//         subs.style.width = '150px'
+//     }, 3100);   
 
-    setTimeout(function(){
-        subs.style.color = '#fff'
-    }, 3400);
+//     setTimeout(function(){
+//         subs.style.color = '#fff'
+//     }, 3400);
     
-    setTimeout(function(){
-        subs.innerHTML = "DONE";
-    }, 3400);
+//     setTimeout(function(){
+//         subs.innerHTML = "DONE";
+//     }, 3400);
 
-    setTimeout(function(){
-        subs.innerHTML = "SUBSCRIBE";
-    }, 5000); 
+//     setTimeout(function(){
+//         subs.innerHTML = "SUBSCRIBE";
+//     }, 5000); 
 
     
-}
+// }
 
 
 
