83a25c201beec0ed4f3571b3b7b8bb9683606515
Deep <57810645+d-night-owl@users.noreply.github.com>
2021-07-10 14:55:07 +0530

    fixes

--- crowdfunding/JS/testzero.js
+++ crowdfunding/JS/testzero.js
@@ -1,5 +1,6 @@
 $("#close-process").click(function(){
 
+    // document.getElementById("text").value = ''
     $(".slabs").removeClass("slab-active");
     // $(".slabs i").removeClass("arrow-active");
     $(".slab-1").click()
@@ -7,19 +8,21 @@ $("#close-process").click(function(){
       
       $('.slab-1').addClass("slab-active");
     //   $('.slab-1').find("i").addClass("arrow-active");
+
+    
 });
       
       
-      $(".slab-1").click(function(){
-        document.getElementById("slab-name").innerHTML = "cones"
+    $(".slab-1").click(function(){
+        document.getElementById("slab-name").innerHTML = "1C ENGINE"
         document.getElementById("payment-amount").innerHTML = "100.00"
         // document.getElementById("qr-generate").src = "./QRS/QR.png"
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=100&tn=Velocita%20Racing%20Crowdfunding%20Cones";
-        document.getElementById("dis-one").value = "CONES"
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=100&tn=Velocita%20Racing%20Crowdfunding%201CENGINE";
+        document.getElementById("dis-one").value = "1C ENGINE"
         document.getElementById("dis-two").value = "100"
         $("#edit-money").css({'display': 'none'});
 
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=100&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=100&tn=Velocita%20Racing%20Crowdfunding%201cENGINE")
 
         document.getElementById("click-here").innerHTML = '<a href="#ex1" rel="modal:open">Click here</a>'
         
@@ -42,17 +45,18 @@ $("#close-process").click(function(){
               $("#qr-generate").css({'display':'block'});
             }, 2800);
 
-        document.getElementById('worth-details').innerHTML='cones worth INR 100'
-      });
-      $(".slab-2").click(function(){
-        document.getElementById("slab-name").innerHTML = "flag"
+        document.getElementById('worth-details').innerHTML='1C engine worth INR 100'
+    });
+
+    $(".slab-2").click(function(){
+        document.getElementById("slab-name").innerHTML = "V2 (V-TWIN)"
         document.getElementById("payment-amount").innerHTML = "500.00"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=500&tn=Velocita%20Racing%20Crowdfunding%20Flag"
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=500&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=500&tn=Velocita%20Racing%20Crowdfunding%20V2V-TWIN"
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=500&tn=Velocita%20Racing%20Crowdfunding%20V2V-TWIN")
 
         document.getElementById("dis-two").value = "500"
-        document.getElementById("dis-one").value = "FLAG"
+        document.getElementById("dis-one").value = "V2 V-TWIN"
         $("#edit-money").css({'display': 'none'});
         // document.getElementById("click-here").innerHTML = '<a href="#ex2" rel="modal:open">Click here</a>'
         $("#qr-generate").css({'display':'none'});
@@ -73,18 +77,19 @@ $("#close-process").click(function(){
             setTimeout(function(){
               $("#qr-generate").css({'display':'block'});
             }, 2800);
-            document.getElementById('worth-details').innerHTML='flag worth INR 500'
-      });
-      $(".slab-3").click(function(){
-        document.getElementById("slab-name").innerHTML = "aero"
+            document.getElementById('worth-details').innerHTML='V2 V-Twin worth INR 500'
+    });
+
+    $(".slab-3").click(function(){
+        document.getElementById("slab-name").innerHTML = "I4 (INLINE 4)"
         document.getElementById("payment-amount").innerHTML = "1,000.00"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1000&tn=Velocita%20Racing%20Crowdfunding%20Cones"
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1000&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1000&tn=Velocita%20Racing%20Crowdfunding%20I4-INLINE-4"
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1000&tn=Velocita%20Racing%20Crowdfunding%20I4-INLINE-4")
 
-        document.getElementById("dis-one").value = "AERO"
+        document.getElementById("dis-one").value = "I4 INLINE 4"
         document.getElementById("dis-two").value = "1000"
-        document.getElementById('worth-details').innerHTML='aero worth INR 1,000'
+        document.getElementById('worth-details').innerHTML='I4 INLINE 4 worth INR 1,000'
         $("#edit-money").css({'display': 'none'});
         $("#qr-generate").css({'display':'none'});
         
@@ -104,17 +109,18 @@ $("#close-process").click(function(){
             }, 2800);
 
             
-      });
-      $(".slab-4").click(function(){
-        document.getElementById("slab-name").innerHTML = "power"
+    });
+
+    $(".slab-4").click(function(){
+        document.getElementById("slab-name").innerHTML = "V6 engine"
         document.getElementById("payment-amount").innerHTML = "1,500.00"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1500&tn=Velocita%20Racing%20Crowdfunding%20Cones"
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1500&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1500&tn=Velocita%20Racing%20Crowdfunding%20V6Engine"
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1500&tn=Velocita%20Racing%20Crowdfunding%20V6Engine")
 
-        document.getElementById("dis-one").value = "POWER"
+        document.getElementById("dis-one").value = "V6 ENGINE"
         document.getElementById("dis-two").value = "1500"
-        document.getElementById('worth-details').innerHTML='power worth INR 1,500'
+        document.getElementById('worth-details').innerHTML='V6 Engine worth INR 1,500'
         $("#edit-money").css({'display': 'none'});
         $("#qr-generate").css({'display':'none'});
         
@@ -133,17 +139,18 @@ $("#close-process").click(function(){
               $("#qr-generate").css({'display':'block'});
             }, 2800);
         // document.getElementById('myImage').src='./gift-2.gif'
-      });
-      $(".slab-5").click(function(){
-        document.getElementById("slab-name").innerHTML = "radiator"
+    });
+
+    $(".slab-5").click(function(){
+        document.getElementById("slab-name").innerHTML = "V8 Engine"
         document.getElementById("payment-amount").innerHTML = "3,000.00"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=3000&tn=Velocita%20Racing%20Crowdfunding%20Cones"
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=3000&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=3000&tn=Velocita%20Racing%20Crowdfunding%20V8Engine"
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=3000&tn=Velocita%20Racing%20Crowdfunding%20V8Engine")
 
-        document.getElementById("dis-one").value = "RADIATOR"
+        document.getElementById("dis-one").value = "V8 ENGINE"
         document.getElementById("dis-two").value = "3000"
-        document.getElementById('worth-details').innerHTML='raditor worth INR 3,000'
+        document.getElementById('worth-details').innerHTML='V8 Engine worth INR 3,000'
         $("#edit-money").css({'display': 'none'});
         $("#qr-generate").css({'display':'none'});
         
@@ -162,19 +169,20 @@ $("#close-process").click(function(){
               $("#qr-generate").css({'display':'block'});
             }, 2800);
         // document.getElementById('myImage').src='./gift-2.gif'
-      });
-      $(".slab-6").click(function(){
-        document.getElementById("slab-name").innerHTML = "side pods"
+    });
+
+    $(".slab-6").click(function(){
+        document.getElementById("slab-name").innerHTML = "V10 Engine"
         document.getElementById("payment-amount").innerHTML = "5,000.00"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=5000&tn=Velocita%20Racing%20Crowdfunding%20Cones"
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=5000&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=5000&tn=Velocita%20Racing%20Crowdfunding%20V10Engine"
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=5000&tn=Velocita%20Racing%20Crowdfunding%20V10Engine")
 
-        document.getElementById("dis-one").value = "SIDE PODS"
+        document.getElementById("dis-one").value = "V10 ENGINE"
         document.getElementById("dis-two").value = "5000"
         $("#edit-money").css({'display': 'none'});
 
-        document.getElementById('worth-details').innerHTML='side pods worth INR 5,000'
+        document.getElementById('worth-details').innerHTML='V10 Engine worth INR 5,000'
         $("#qr-generate").css({'display':'none'});
         
 
@@ -192,19 +200,20 @@ $("#close-process").click(function(){
               $("#qr-generate").css({'display':'block'});
             }, 2800);
         // document.getElementById('myImage').src='./gift-2.gif'
-      });
-      $(".slab-7").click(function(){
-        document.getElementById("slab-name").innerHTML = "wings"
+    });
+
+    $(".slab-7").click(function(){
+        document.getElementById("slab-name").innerHTML = "V12 Engine"
         document.getElementById("payment-amount").innerHTML = "10,000.00"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("dis-one").value = "WINGS"
+        document.getElementById("dis-one").value = "V12 ENGINE"
         document.getElementById("dis-two").value = "10000"
         $("#edit-money").css({'display': 'none'});
-        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=10000&tn=Velocita%20Racing%20Crowdfunding%20Cones"
-        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1000&tn=Velocita%20Racing%20Crowdfunding%20Cones")
+        document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=10000&tn=Velocita%20Racing%20Crowdfunding%20V12Engine"
+        $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=1000&tn=Velocita%20Racing%20Crowdfunding%20V12Engine")
 
 
-        document.getElementById('worth-details').innerHTML='wings worth INR 10,000'
+        document.getElementById('worth-details').innerHTML='V12 Engine worth INR 10,000'
         $("#qr-generate").css({'display':'none'});
         
 
@@ -222,13 +231,15 @@ $("#close-process").click(function(){
               $("#qr-generate").css({'display':'block'});
             }, 2800);
         // document.getElementById('myImage').src='./gift-2.gif'
-      });
-      $(".slab-8").click(function(){
-        document.getElementById("slab-name").innerHTML = "XXXX"
+    });
+
+    $(".slab-8").click(function(){
+        document.getElementById("slab-name").innerHTML = "OPEN SLAB"
         document.getElementById("payment-amount").innerHTML = "XXXX.XX"
         // document.getElementById("qr-generate").src = "./QRS/QR100.png"
-        document.getElementById("dis-one").value = "XXXX"
+        document.getElementById("dis-one").value = "OPEN SLAB"
         $("#edit-money").css({'display': 'block'});
+        document.getElementById("dis-two").value = "100"
         $("#edit-money").click (function() {
           $('#dis-two').prop('disabled', false);
         });
@@ -240,9 +251,9 @@ $("#close-process").click(function(){
 
         $("#proceed-step-three").click (function() {
           var nameValue = document.getElementById("dis-two").value;
-          document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am="+ nameValue + "&tn=Velocita%20Racing%20Crowdfunding%20Cones"
-          $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=" + nameValue + "&tn=Velocita%20Racing%20Crowdfunding%20Cones");
-
+          var amtValue = document.getElementById("dis-one").value;
+          document.getElementById("text").value = "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am="+ nameValue + "&tn=Velocita%20Racing%20Crowdfunding%20Slab" + amtValue
+          $("#mobile-upi").attr("href", "upi://pay?cu=INR&pa=satvikcrvk718-1@oksbi&pn=Satvik%20S.&am=" + nameValue + "&tn=Velocita%20Racing%20Crowdfunding%20Slab" + amtValue);
         });
 
        
@@ -250,7 +261,7 @@ $("#close-process").click(function(){
         
 
         
-        document.getElementById('worth-details').innerHTML='XXX worth INR XXX'
+        document.getElementById('worth-details').innerHTML='Open Slab. You can edit amount details on checkout'
         $("#qr-generate").css({'display':'none'});
         
 
